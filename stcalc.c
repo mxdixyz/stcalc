a220332697115f41710a3f4817a54ef71f4a90c5
Madison Lynch <madi@mxdi.xyz>
2023-01-15 01:06:41 -0800

    [release 2.0] * added modulo operations * added documentation * eof (ctr+d) used to exit (instead of ctrl+c)

--- stcalc.c
+++ stcalc.c
@@ -1,25 +1,32 @@
-/* See LICENSE file for copyright and license details. */
+// See LICENSE file for copyright and license details.
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
+
+void prevAns(char *inp1,char *inp2);
+
+static double num1,num2,ans;
 
 int
 main(int argc,char *argv[]) {
+	// Check for arguments
 	if(argc>1) {
 		if(!strcmp(argv[1],"-h")) {
 			printf("Improper Usage:\n> ""\033[31m""1+2""\033[37m""\nProper Usage:\n> ""\033[32m""1 + 2\n""\033[37m");
-			return 0;
+			return EXIT_SUCCESS;
 		} else {
 			printf("Only passable argument if \"-h,\" for help\n");
-			return 1;
+			return EXIT_FAILURE;
 		}
 	}
-	double num1,num2,ans;
-	char inp1[5],inp2[5],op;
-	int ansSet,err,eof;
+	// Disable Ctrl+C
+	signal(SIGINT,SIG_IGN);
 	system("clear");
-	printf("Operators: [+, -, *, /]\nType \"ans\" for previous answer\n(Ctrl + C) - exit\n\n\n\n\n");
-	for(;;) {
+	int ansSet,err,eof;
+	char inp1[16],inp2[16],op;
+	printf("Operators: [+, -, *, /, %%]\nInput \"ans\" for previous answer\n(Ctrl+D) to exit\n\n\n\n\n");
+	while(eof!=EOF) {
 		if(ansSet) {
 			if(err) {
 				printf("\033[31m""Invalid operator\n""\033[37m");
@@ -27,25 +34,10 @@ main(int argc,char *argv[]) {
 				printf("Answer: %.2lf\n",ans);
 			}
 		}
-		err=0;
+		err=0;	
 		printf("> ");
 		eof=scanf("%s %c %s",inp1,&op,inp2);
-		if(eof==EOF) {
-			return 0;
-		}
-		if(!strcmp(inp1,"ans")&&!strcmp(inp2,"ans")) {
-			num1=ans;
-			num2=ans;
-		} else if(!strcmp(inp1,"ans")) {
-			num1=ans;
-			num2=atoi(inp2);
-		} else if(!strcmp(inp2,"ans")) {
-			num1=atoi(inp1);
-			num2=ans;
-		} else {
-			num1=atoi(inp1);
-			num2=atoi(inp2);
-		}
+		prevAns(inp1,inp2);
 		switch(op) {
 			case '+':
 				ans=num1+num2;
@@ -59,10 +51,41 @@ main(int argc,char *argv[]) {
 			case '/':
 				ans=num1/num2;
 				break;
+			case '%':
+				ans=(double)((int)num1%(int)num2);
+				break;
 			default:
 				err=1;
-				break;
 		}
 		ansSet=1;
 	}	
+	printf("\n");
+	return EXIT_SUCCESS;
+}
+
+/** @brief Convert string input to double.
+ *
+ *  If user inputs "ans" for either input, the
+ *  corresponding variable will be set to the
+ *  previous answer.
+ *
+ *  @param inp1 First number given by user.
+ *  @param inp2 Second number given by user.
+ *  @return Void.
+ */
+void
+prevAns(char *inp1,char *inp2) {
+	if(!strcmp(inp1,"ans")&&!strcmp(inp2,"ans")) {
+		num1=ans;
+		num2=ans;
+	} else if(!strcmp(inp1,"ans")) {
+		num1=ans;
+		num2=atoi(inp2);
+	} else if(!strcmp(inp2,"ans")) {
+		num1=atoi(inp1);
+		num2=ans;
+	} else {
+		num1=atoi(inp1);
+		num2=atoi(inp2);
+	}
 }
